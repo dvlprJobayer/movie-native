3e2bee6b6ada7307937f6e38da9dab28b0dde0fe
Jobayer Ahammed <nesarahammed019@gamil.com>
2024-02-03 23:56:29 +0600

    Trending Movies Carousel done

---
+++ components/TrendingMovies.tsx
@@ -0,0 +1,38 @@
+import { Dimensions, Image, Pressable, Text, View } from "react-native";
+import Carousel from "react-native-snap-carousel";
+import tw from "twrnc";
+
+interface TrendingMoviesProps {
+    data: number[]
+}
+
+const {width, height} = Dimensions.get("window");
+
+const TrendingMovies = ({data}: TrendingMoviesProps) => {
+    return (
+        <View style={tw`mb-8`}>
+            <Text style={tw`text-white text-xl mx-4 mb-5`}>Trending</Text>
+            <Carousel
+                data={data}
+                renderItem={({item}) => <MovieCard item={item} />}
+                inactiveSlideOpacity={0.6}
+                sliderWidth={width}
+                itemWidth={width * 0.62}
+                slideStyle={{display: "flex", alignItems: "center"}}
+            />
+        </View>
+    );
+}
+
+export default TrendingMovies;
+
+const MovieCard = ({item}: {item: number}) => {
+    return (
+        <Pressable>
+            <Image 
+                source={require("../assets/images/moviePoster1.png")} 
+                style={[{width: width * 0.6, height: height * 0.4}, tw`rounded-3xl`]}
+            />
+        </Pressable>
+    )
+}
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,12 +2,18 @@ import {View, Text, SafeAreaView, TouchableOpacity, ScrollView} from "react-nati
 import tw from "twrnc";
 import { styles } from "../theme";
 import {Bars3CenterLeftIcon, MagnifyingGlassIcon} from "react-native-heroicons/outline";
+import TrendingMovies from "../components/TrendingMovies";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 
 const HomeScreen = () => {
+    const [trending, setTrending] = useState([1, 2, 3]);
+
     return (
         <SafeAreaView style={tw`flex-1 bg-neutral-800`}>
+            <StatusBar style="light" />
             {/* Search bar, Menu Icon and logo */}
-            <View style={tw`flex-row justify-between items-center mx-4 mt-2`}>
+            <View style={tw`flex-row justify-between items-center mx-4 mt-8 mb-5`}>
                 <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
                 <Text 
                     style={tw`text-white text-3xl font-bold`}>
@@ -18,7 +24,13 @@ const HomeScreen = () => {
                 </TouchableOpacity>
             </View>
 
-            
+            {/* Movies Section */}
+            <ScrollView
+                contentContainerStyle={{paddingBottom: 10}}
+            >
+                {/* Trending Movies Carousel */}
+                <TrendingMovies data={trending} />
+            </ScrollView>
         </SafeAreaView>
     );
 }
